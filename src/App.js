fa1630083741340df03ca8b5b13973c14ce1ba66
StephenGrable1 <stephengrable@gmail.com>
2018-01-23 16:43:16 -0500

    add new progress bar for email input

--- src/App.js
+++ src/App.js
@@ -9,13 +9,16 @@ import {verifyEmail} from "./VerifyEmail/VerifyEmail.js"
 class App extends Component {
   constructor(props){
     super(props);
-    this.state = {email: ''};
+    this.state = {
+      email: '', 
+      isValidEmail: false
+  };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
   handleSubmit(e) {
     var emailVerified = verifyEmail(this.state.email);
-    console.log(emailVerified)
+    
     if(emailVerified){
       console.log('valid email', this.state.email)
     } else {
@@ -27,13 +30,51 @@ class App extends Component {
 
   handleChange(e){
     this.setState({email: e.target.value})
+    this.setState({isValidEmail: verifyEmail(e.target.value)})
   }
+
   render() {
+    var emailValidationBar = () => {
+      var isEmailValid = this.state.isValidEmail;
+      var emailLength = this.state.email.length;
+
+
+      if(isEmailValid){
+        const barStyle = {
+          width: '100%'
+        }
+        return (
+          <div style={barStyle} className="email-input-bar-outter email-success"></div>
+          
+        )
+      } 
+
+      
+      if(!isEmailValid && (emailLength >= 0 && emailLength <= 0)){
+        const barStyle = {
+          width: '30%'
+        }
+        return (
+          <div style={barStyle}  className="email-input-bar-outter get-started"></div>
+        )
+      }
+
+
+      if(!isEmailValid && (emailLength >= 1)){
+        const barStyle = {
+          width: '70%'
+        }
+        return (
+          <div style={barStyle} className="email-input-bar-outter keep-going" ></div>
+        )
+      } 
+      
+    }
     return (
       <div className="App">
         <header className="App-header">
           <img src={deafboy} className="App-logo" alt="logo" />
-          <h1 className="App-title">deafBoy -/+</h1>
+          <h1 className="App-title">deafBoyUSA</h1>
         </header>
         <div>
         <form onSubmit={this.handleSubmit}>
@@ -42,6 +83,11 @@ class App extends Component {
           </label>
           <input type="submit" value="Submit"></input>
         </form>
+
+        <div className="email-signup-progress-bar">
+          {emailValidationBar()}
+        </div>  
+        <h4>Progress</h4>
         </div>
       </div>
     );
