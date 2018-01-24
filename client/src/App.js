9082ed1f29cbb23097332b23bbe89ac0d97b3551
StephenGrable1 <stephengrable@gmail.com>
2018-01-24 17:07:57 -0500

    get api working from inside app component

--- client/src/App.js
+++ client/src/App.js
@@ -24,8 +24,20 @@ class App extends Component {
   }
 
   componentDidMount() {
-   
+   this.callApi()
+        .then(res => console.log(res))
+        .catch(err => console.log("ERROR:", err))
   }
+
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+    return body;
+  };
+
+
   handleSubmit(e) {
     var emailVerified = verifyEmail(this.state.email);
 
