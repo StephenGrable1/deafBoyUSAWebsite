3bfc85dc0ea5b5da91165c826bb3f567e99b28fa
StephenGrable1 <stephengrable@gmail.com>
2018-01-24 17:16:42 -0500

    get drip to do basic signup for user to drip

--- server.js
+++ server.js
@@ -3,8 +3,32 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const client = require('drip-nodejs')({
+    token:'8f073dbe3865832d8c40ef52cd0a9fdb',
+    accountId: '5542548'
+});
+
+const payload = {
+    subscribers: [{
+      email: "Stephen@acme.com",
+      time_zone: "America/Los_Angeles",
+      custom_fields: {
+        name: "John Doe"
+      }
+    }]
+  };
+
+
 app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express Ya' });
+    client.createUpdateSubscriber(payload)
+    .then((res) => {
+        console.log(res)
+      // Handle `response.body`
+    })
+    .catch((error) => {
+        console.log(error)
+      // Handle errors
+    });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
